f592e05c4183bc5d1571ebc71a8ee5aa261ae103
Paúl Valverde <paul-vlvrd@hotmail.com>
2023-08-23 14:48:06 -0400

    Refactoring project

---
+++ battery/spindler_battery.py
@@ -0,0 +1,11 @@
+from battery.battery import Battery
+from utils.date_utils import add_years_to_date
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        date_next_battery_service = add_years_to_date(self.last_service_date, 2)
+        return date_next_battery_service < self.current_date
